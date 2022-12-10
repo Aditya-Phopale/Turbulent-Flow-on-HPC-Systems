188b1771d9ec0af3df71d6b4a393fe1aeca898fe
jefin_solomon <jefin.jayasingh-powlin@tum.de>
2022-12-10 13:13:07 +0100

    All viscosity stencils completed, inherited from TurbulentFlowField

--- Source/Stencils/ViscosityBufferFillStencil.hpp
+++ Source/Stencils/ViscosityBufferFillStencil.hpp
@@ -3,5 +3,35 @@
 #include <vector>
 
 #include "BoundaryStencil.hpp"
-#include "FlowField.hpp"
+#include "TurbulentFlowField.hpp"
 #include "Parameters.hpp"
+
+namespace Stencils {
+
+  class ViscosityBufferFillStencil: public BoundaryStencil<TurbulentFlowField> {
+  private:
+    std::vector<RealType>& Left_;
+    std::vector<RealType>& Right_;
+    std::vector<RealType>& Top_;
+    std::vector<RealType>& Bottom_;
+    std::vector<RealType>& Front_;
+    std::vector<RealType>& Back_;
+
+  public:
+    ViscosityBufferFillStencil(const Parameters&, std::vector<RealType>&, std::vector<RealType>&, std::vector<RealType>&, std::vector<RealType>&, std::vector<RealType>&, std::vector<RealType>&);
+    ~ViscosityBufferFillStencil() override = default;
+
+    void applyLeftWall(TurbulentFlowField& turbFlowField, int i, int j) override;
+    void applyRightWall(TurbulentFlowField& turbFlowField, int i, int j) override;
+    void applyBottomWall(TurbulentFlowField& turbFlowField, int i, int j) override;
+    void applyTopWall(TurbulentFlowField& turbFlowField, int i, int j) override;
+
+    void applyLeftWall(TurbulentFlowField& turbFlowField, int i, int j, int k) override;
+    void applyRightWall(TurbulentFlowField& turbFlowField, int i, int j, int k) override;
+    void applyBottomWall(TurbulentFlowField& turbFlowField, int i, int j, int k) override;
+    void applyTopWall(TurbulentFlowField& turbFlowField, int i, int j, int k) override;
+    void applyFrontWall(TurbulentFlowField& turbFlowField, int i, int j, int k) override;
+    void applyBackWall(TurbulentFlowField& turbFlowField, int i, int j, int k) override;
+
+  };
+} // namespace Stencils
